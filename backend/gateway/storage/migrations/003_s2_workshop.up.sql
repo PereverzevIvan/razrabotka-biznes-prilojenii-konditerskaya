501a4fc325c21b38271d6ba0639b5660d1d13bbe
PereverzevIvan <peregh320@gmail.com>
2024-11-17 16:34:59 +0300

    Доработки миграции и мигратора

--- backend/gateway/storage/migrations/003_s2_workshop.up.sql
+++ backend/gateway/storage/migrations/003_s2_workshop.up.sql
@@ -1,49 +1,42 @@
-
 -- Цеха
-
 drop table if exists `workshops`;
-create table `workshops`  (
-    `id` int(11) NOT NULL AUTO_INCREMENT,
-    
-    `name` varchar(255) NOT NULL,    
-    `scheme_image` varchar(255) NOT NULL,
-    
-    `rotated` tinyint(1) NOT NULL,
 
-    PRIMARY KEY (`id`),
-    UNIQUE (`name`)
+create table `workshops` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `scheme_image` varchar(255) NOT NULL,
+  `rotated` tinyint (1) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE (`name`)
 );
 
 drop table if exists `workshop_object_types`;
-create table `workshop_object_types`  (
-    `id` int(11) NOT NULL AUTO_INCREMENT,
-    
-    `name` varchar(255) NOT NULL,
-    `image` varchar(255) NOT NULL,
-    
-    PRIMARY KEY (`id`),
-    UNIQUE (`name`)
+
+create table `workshop_object_types` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `image` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE (`name`)
 );
 
-insert into `workshop_object_types` (`id`, `name`, `image`) values
-    (1, "Equipment", "equipment.png"),
-    (2, "Exit", "exit.jpg"),
-    (3, "Fire extinguisher", "FireExtinguisher.png"),
-    (4, "First aid", "FirstAid.png")
-    ;
-    
+insert into
+  `workshop_object_types` (`id`, `name`, `image`)
+values
+  (1, "Equipment", "equipment.png"),
+  (2, "Exit", "exit.jpg"),
+  (3, "Fire extinguisher", "FireExtinguisher.png"),
+  (4, "First aid", "FirstAid.png");
 
 drop table if exists `workshop_objects`;
-create table `workshop_objects`  (
-    `id` int(11) NOT NULL AUTO_INCREMENT,
 
-    `workshop_id` int(11) NOT NULL,
-    `object_type_id` int(11) NOT NULL,
-
-    `x` DOUBLE PRECISION NOT NULL,
-    `y` DOUBLE PRECISION NOT NULL,
-    
-    PRIMARY KEY (`id`),
-    FOREIGN KEY (`workshop_id`) REFERENCES `workshops` (`id`)
-    FOREIGN KEY (`type_id`) REFERENCES `workshop_object_types` (`id`)
+create table `workshop_objects` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `workshop_id` int(11) NOT NULL,
+  `object_type_id` int(11) NOT NULL,
+  `x` DOUBLE PRECISION NOT NULL,
+  `y` DOUBLE PRECISION NOT NULL,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`workshop_id`) REFERENCES `workshops` (`id`),
+  FOREIGN KEY (`object_type_id`) REFERENCES `workshop_object_types` (`id`)
 );
--- backend/gateway/storage/migrations/005_s2_quality_control.up.sql
+++ backend/gateway/storage/migrations/005_s2_quality_control.up.sql
@@ -1,20 +1,16 @@
 -- Контроль качества
+drop table if exists `quality_controls`;
 
-drop table `quality_control`;
-create table `quality_control`  (
-    `id` int(11) NOT NULL AUTO_INCREMENT,
-    
-    `order_id` int(11) NOT NULL,
-    `master_id` int(11) NOT NULL,
-
-    `is_satisfies` tinyint(1) NOT NULL,
-    `comment` varchar(255) DEFAULT NULL,
-    
-    `parameter` varchar(255) NOT NULL,
-    -- Или использовать FK по таблице `parameters`
-    -- `parameter_id` int(11) NOT NULL,
-    
-    PRIMARY KEY (`id`),
-    FOREIGN KEY (`order_id`) REFERENCES `orders` (`id`),
-    FOREIGN KEY (`master_id`) REFERENCES `users` (`id`)
+create table `quality_controls` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `order_id` int(11) NOT NULL,
+  `master_id` int(11) NOT NULL,
+  `is_satisfies` tinyint (1) NOT NULL,
+  `comment` varchar(255) DEFAULT NULL,
+  `parameter` varchar(255) NOT NULL,
+  -- Или использовать FK по таблице `parameters`
+  -- `parameter_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`order_id`) REFERENCES `orders` (`id`),
+  FOREIGN KEY (`master_id`) REFERENCES `users` (`id`)
 );
